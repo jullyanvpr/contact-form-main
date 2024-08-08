d99ca1d075939f345f6ebb08770c403e376bfc54
Jullyan Gomes <jullyanvpr08@gmail.com>
2024-08-08 14:35:15 -0300

    updated readme files and alert message

--- README.md
+++ README.md
@@ -38,8 +38,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [Frontendmentor](https://www.frontendmentor.io/solutions/simple-contact-form-c2tE7TTJ0q)
+- Live Site URL: [Netlify] (https://luminous-trifle-37b565.netlify.app)
 
 ## My process
 
--- script.js
+++ script.js
@@ -6,9 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
     const textarea = document.getElementById('messageBox');
     const charCount = document.getElementById('charCount');
     const maxChars = 450;
-  
+
+    // Atualiza o contador de caracteres enquanto o usuário digita
     textarea.addEventListener('input', function() {
       const remaining = maxChars - textarea.value.length;
       charCount.textContent = `${remaining} characters remaining`;
     });
-  });
+
+    const form = document.getElementById('contactForm');
+    const submitButton = document.getElementById('submitButton');
+    const confirmationMessage = document.getElementById('confirmationMessage');
+
+    // Adiciona evento de submit ao formulário
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Impede o envio padrão do formulário
+
+      if (form.checkValidity()) {
+        // Se o formulário for válido, exibe a mensagem de confirmação
+        confirmationMessage.style.display = 'block';
+        submitButton.style.display = 'none'; // Oculta o botão de envio após o envio
+
+        // Exibe um alerta
+        alert('Formulário enviado!');
+      } else {
+        // Se o formulário não for válido, exibe uma mensagem de erro ou realiza outras ações
+        confirmationMessage.style.display = 'none';
+        submitButton.style.display = 'block'; // Garante que o botão de envio continue visível
+      }
+    });
+});
